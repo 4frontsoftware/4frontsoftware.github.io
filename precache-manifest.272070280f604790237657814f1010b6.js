6be3a88d46398c87fddb2ebe99cac5b59c8c664b
chain16 <chainwork7@gmail.com>
2021-04-09 10:52:48 -0600

    Updates

---
+++ precache-manifest.272070280f604790237657814f1010b6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f8746ac96751890deafbecfb5ec206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5315bdeebbc9fc206cea",
+    "url": "/static/css/main.17ba0ad6.chunk.css"
+  },
+  {
+    "revision": "a52e669c1ec58da86d42",
+    "url": "/static/js/2.d1b37fd0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d1b37fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5315bdeebbc9fc206cea",
+    "url": "/static/js/main.a9395df1.chunk.js"
+  },
+  {
+    "revision": "fe2ada163b2ad92dc54d",
+    "url": "/static/js/runtime-main.55b98cef.js"
+  },
+  {
+    "revision": "9a4ae6e9ff776cbf4ac58f95dbb26e46",
+    "url": "/static/media/ai.9a4ae6e9.png"
+  },
+  {
+    "revision": "d2deb990e1f30158146605e2c372f98b",
+    "url": "/static/media/clouds.d2deb990.jpg"
+  },
+  {
+    "revision": "b4662c2df8b4c663bbdcbcc5398c9a51",
+    "url": "/static/media/covid.b4662c2d.jpg"
+  },
+  {
+    "revision": "1d53f3095819c88913f5dc32bd735667",
+    "url": "/static/media/facebook.1d53f309.svg"
+  },
+  {
+    "revision": "ea50de2d3a2f20b553306420c11c9337",
+    "url": "/static/media/gps.ea50de2d.jpg"
+  },
+  {
+    "revision": "7d30d64ed18c076570544b50c5076956",
+    "url": "/static/media/graphs.7d30d64e.jpg"
+  },
+  {
+    "revision": "7de5393d90d706a34a722a718f9510c8",
+    "url": "/static/media/hamburger.7de5393d.svg"
+  },
+  {
+    "revision": "15708d58d74c5267264bb925556948ce",
+    "url": "/static/media/home-bk.15708d58.jpg"
+  },
+  {
+    "revision": "22e4152035398724deaca24a253b5af1",
+    "url": "/static/media/kitchen.22e41520.jpg"
+  },
+  {
+    "revision": "0a0855b71a169d1bb39528305dad14f9",
+    "url": "/static/media/linkedin.0a0855b7.svg"
+  },
+  {
+    "revision": "21901cddff4f6cca21235f47bc918ef2",
+    "url": "/static/media/logo.21901cdd.svg"
+  },
+  {
+    "revision": "2ec324f723b341d2fe32275c1a1fd975",
+    "url": "/static/media/mountainImg.2ec324f7.jpg"
+  },
+  {
+    "revision": "31c1386344f32eb23170b46fbf2c5766",
+    "url": "/static/media/twitter.31c13863.svg"
+  }
+]);
